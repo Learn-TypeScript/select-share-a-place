125d2c9d8c7c6607c1d1376873219512ccd19b6f
fotis tsakiris <footios76@gmail.com>
2020-10-12 01:50:24 +0300

    get user input and fetch coordinates

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ Created by Maximilian Schwarzmüller
 
 - This is the continue of the `drug-n-drop` repo.
 
+Take care of billing account in Google Cloud Platform, after finishing this app.
+
 ### Notes:
 
 13. **Time to Practice! Let's build a "Select & Share a Place" App (incl. Google Maps)**
+
+- rm -rf .git => to remove the version control we downloaded.
+
+- (170). Setting Up a Google API Key
+- Check Q&A [Alternative to Google Geocoding API](https://www.udemy.com/course/understanding-typescript/learn/lecture/16936070#questions/8913424) about [Nominatim](http://nominatim.org/release-docs/latest/api/Search/)
+- (171). Using Axios to Fetch Coordinates for an Entered Address
+- [Status codes](https://developers.google.com/maps/documentation/geocoding/overview#StatusCodes)
+  The "status" field within the Geocoding response object contains the status of the request, and may contain debugging information to help you track down why geocoding is not working.
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-// Code goes here!
+import axios from "axios";
+
+import { API_KEY } from "../google.api.key";
+
+type GoogleGeoCodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+const form = document.querySelector("form")!;
+const addressInput = document.querySelector("#address")! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeoCodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${API_KEY}`
+    )
+    .then(response => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
